0fe6ab2761fd8bcfd733f76e0c84459db7dbf6ce
Courtney Suhr <courtneylsuhr@gmail.com>
2026-01-13 14:42:55 +0000

    homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import Layout from "./components/Layout";
 import ValuesPage from "./pages/ValuesPage";
 import TeamPage from "./pages/TeamPage";
